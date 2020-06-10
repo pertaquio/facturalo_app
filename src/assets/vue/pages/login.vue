93ebe75096a76ec9caad7113b3e2e6e66754e8c9
Raul <sh.raul.jgb@gmail.com>
2020-06-09 20:11:07 -0500

    login

--- src/assets/vue/pages/login.vue
+++ src/assets/vue/pages/login.vue
@@ -1,76 +1,141 @@
 <template>
-  <f7-page>
-    <f7-navbar class="navbar color-theme-white no-shadow theme-dark" title="FacturaloPeru APP"></f7-navbar>
-
-    <f7-block-title class="title">Inicio de Sesión</f7-block-title>
-
-    <form class="list form-store-data" id="demo-form">
-      <ul>
-        <li class="item-content item-input">
-          <div class="item-inner">
-            <div class="item-title item-label">Usuario</div>
-            <div class="item-input-wrap">
-              <input
-                name="name"
-                required
-                validate
-                type="text"
-                v-model="email"
-                placeholder="Ingrese usuario"
-              />
-              <span class="input-clear-button"></span>
-            </div>
-          </div>
-        </li>
-        <li class="item-content item-input">
-          <div class="item-inner">
-            <div class="item-title item-label">Password</div>
-            <div class="item-input-wrap">
-              <input
-                name="password"
-                required
-                validate
-                v-model="password"
-                type="password"
-                placeholder="Ingrese password"
-              />
-              <span class="input-clear-button"></span>
-            </div>
-          </div>
-        </li>
-        <li class="item-content item-input item-input-with-info">
-          <div class="item-inner">
-            <div class="item-title item-label">URL</div>
-            <div class="item-input-wrap">
-              <input
-                type="url"
-                placeholder="https://demo.facturador.pro"
-                required
-                validate
-                v-model="url"
-              />
-              <span class="input-clear-button"></span>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </form>
+  <f7-page class="page-red" color="white">
     <f7-block>
       <f7-row>
-        <f7-col>
-          <f7-button color="green" @click="login" outline>Ingresar</f7-button>
+        <f7-col width="100">
+          <p class="text-align-center">
+            <img :src="fp_logo_white" alt="FacturaloPeru" width="60%" />
+          </p>
+          <p class="text-align-center text-color-white padding-top">BIENVENIDO</p>
         </f7-col>
+      </f7-row>
+    </f7-block>
+
+    <f7-card color="red">
+      <f7-card-content>
+        <f7-row no-gap>
+          <f7-col>
+            <form class="form-store-data" id="demo-form">
+              <f7-list no-hairlines-md>
+                <f7-list-input
+                  outline
+                  floating-label
+                  type="text"
+                  clear-button
+                  label="Correo"
+                  placeholder="Correo"
+                  name="name"
+                  required
+                  validate
+                  :value="email"
+                  @input="email = $event.target.value"
+                >
+                </f7-list-input>
+                <f7-list-input
+                  outline
+                  floating-label
+                  type="password"
+                  clear-button
+                  label="Contraseña"
+                  placeholder="Contraseña"
+                  name="password"
+                  required
+                  validate
+                  :value="password"
+                  @input="password = $event.target.value"
+                >
+                </f7-list-input>
+                <f7-list-input
+                  outline
+                  floating-label
+                  type="url"
+                  clear-button
+                  label="URL"
+                  placeholder="https://demo.facturador.pro"
+                  required
+                  validate
+                  :value="url"
+                  @input="url = $event.target.value"
+                >
+                </f7-list-input>
+              </f7-list>
+            </form>
+          </f7-col>
+        </f7-row>
+      </f7-card-content>
+    </f7-card>
+
+    <!-- <f7-block>
+      <f7-row no-gap>
+      <form class="form-store-data" id="demo-form">
+
+        <ul>
+          <li class="item-content item-input">
+            <div class="item-inner">
+              <div class="item-title item-label">Usuario</div>
+              <div class="item-input-wrap">
+                <input
+                  name="name"
+                  required
+                  validate
+                  type="text"
+                  v-model="email"
+                  placeholder="Ingrese usuario"
+                />
+                <span class="input-clear-button"></span>
+              </div>
+            </div>
+          </li>
+          <li class="item-content item-input">
+            <div class="item-inner">
+              <div class="item-title item-label">Password</div>
+              <div class="item-input-wrap">
+                <input
+                  name="password"
+                  required
+                  validate
+                  v-model="password"
+                  type="password"
+                  placeholder="Ingrese password"
+                />
+                <span class="input-clear-button"></span>
+              </div>
+            </div>
+          </li>
+          <li class="item-content item-input item-input-with-info">
+            <div class="item-inner">
+              <div class="item-title item-label">URL</div>
+              <div class="item-input-wrap">
+                <input
+                  type="url"
+                  placeholder="https://demo.facturador.pro"
+                  required
+                  validate
+                  v-model="url"
+                />
+                <span class="input-clear-button"></span>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </form>
+      </f7-row>
+    </f7-block> -->
+
+    <f7-block>
+      <f7-row>
+        <f7-col width="20"></f7-col>
+        <f7-col width="60">
+          <f7-button @click="login" color="red" fill raised>Ingresar</f7-button>
+        </f7-col>
+        <f7-col width="20"></f7-col>
       </f7-row>
     </f7-block>
   </f7-page>
 </template>
 <style scoped>
 
-.navbar-cus {
-  background: #17a2b8;
-  color: white;
-}
-.item-label {
+/*.item-label {
   font-size: 17px;
   font-weight: bold;
 }
@@ -80,10 +145,13 @@
   font-weight: bold;
   font-size: 20px;
   margin-bottom: 20%;
-}
+}*/
 </style>
 <script>
+
 import { auth } from "mixins_/auth";
+import FpLogoDark from "assets/images/facturaloperu.png";
+import FpLogoWhite from "assets/images/fp_logo_white.png";
 
 export default {
   mixins: [auth],
@@ -92,7 +160,9 @@ export default {
     return {
       email: "demo@gmail.com",
       password: "123456",
-      url: "https://demo.facturador.pro"
+      url: "https://demo.facturador.pro",
+      fp_logo_dark: FpLogoDark,
+      fp_logo_white: FpLogoWhite,
     };
   },
   created() {},
